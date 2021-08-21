b6b15d13a3b90c3e0117a90b0c4fd1f4b9c46238
VasylievKostia <vasylievk91@gmail.com>
2021-08-21 13:27:17 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import './sass/main.scss';
-// import menu from './menu.json'
+import menuTemplate from './menu.json'
+import menuList from './tamplates/list.hbs'
 
-console.log('asd')
+// refs
+const menuRef = document.querySelector('.js-menu')
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
 
+// Theme 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+// addition markup
+const markup = menuTemplate.map(menuList).join('')
+menuRef.insertAdjacentHTML('beforeend', markup)
+
+// theme picker
+
+if (localStorage.theme === Theme.DARK) {
+  checkboxRef.checked = true;
+  bodyRef.classList.add(Theme.DARK);
+}
+
+checkboxRef.addEventListener('change', onChangeAction);
+
+// console.log(localStorage.getItem('userSettings', JSON.stringify(Theme)))
+
+function onChangeAction() {
+  if (checkboxRef.checked === true) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyRef.classList.add(Theme.LIGHT);
+    bodyRef.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
